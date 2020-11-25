3f5d973d160f61c99665e86df42471c2daa9e859
ohsh <zasqw456@naver.com>
2020-11-25 18:48:18 +0900

    project ini

---
+++ src/main/kotlin/AppMainKt.kt
@@ -0,0 +1,41 @@
+@file:JvmName("AppMain")
+
+package kr.co.osj4532
+
+
+import ch.qos.logback.classic.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
+import kotlin.system.measureTimeMillis
+
+/**
+ * 201125 | osj4532 | created
+ */
+
+@SpringBootApplication(
+    scanBasePackages = ["kr.co.osj4532"]
+)
+class AppMainKt {
+    companion object {
+        private val log = LoggerFactory.getLogger(AppMainKt::class.java) as Logger
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val elapsed = measureTimeMillis {
+                log.info("============ AppMain Run START ============")
+                val app = runApplication<AppMainKt>(*args) {
+                    setBannerMode(Banner.Mode.OFF)
+                }
+
+                log.info("============ Check Profile START ============")
+                val env = app.getBean("environment") as Environment
+                env.activeProfiles.toList().forEach { log.info(it) }
+                log.info("============ Check Profile START ============ : activate profile count: ${env.activeProfiles.count()}")
+            }
+
+            log.info("============ AppMain Run END ============ : $elapsed ms")
+        }
+    }
+}
